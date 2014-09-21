ce8c7eef84cd6e4b46eaa3d2eab4b0e28c0932a4
000ubird <000ubird@gmail.com>
2014-09-22 00:33:36 +0900

    Mainクラスにシリアライザ検証用の変数を定義
    宣言したのは配列、enum、flash.geom.PointのインポートによるPoint型変数、float型、ハッシュ変数、オブジェクト型変数である。

--- src/Main.hx
+++ src/Main.hx
@@ -1,22 +1,39 @@
 package ;
+
 import haxe.Serializer;
 import haxe.Unserializer;
+import haxe.ds.HashMap;
+import flash.geom.Point;
+
+//enumの例を定義
+enum Color{
+    Red;
+    Green;
+    Blue;
+}
 
 class Main {
     static function main() {
+		//シリアライズ検証用変数の定義
+		var array = new Array<Int>();
+		array[0] = 50;
+		array[5] = 100;
+		var enum_color = Color.Green;
+		var point = new Point(10, 20);
+		var float = 10.55;
+		var hash = new Map();
+		hash.set("abc", 1);
+		
         var object = {
+			color : enum_color,
+			p : point,
             x: 120,
             y: 230,
             z: 990,
+			array: array,
         }
-        trace(object);
-            
+		
         var str = haxe.Serializer.run(object);
-        trace(str);
-        
-        
-        var x = haxe.Unserializer.run(str);
-        trace(x);
-        
+		trace(str);
     }
 }
