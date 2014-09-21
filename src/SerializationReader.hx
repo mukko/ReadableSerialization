16639b70f61562b4b318893df9d8dc00b5c45d70
000ubird <000ubird@gmail.com>
2014-09-22 00:59:21 +0900

    【WIP】デシリアライズされたオブジェクト型の変数をJson形式にして出力するメソッドを定義
    問題点
    ・配列やハッシュが入っている時に正しく出力することができない
    ・型の表示が行えない。
    ・整形したデータをString型の文字列を返す様に変更予定。

--- src/SerializationReader.hx
+++ src/SerializationReader.hx
@@ -1,8 +1,10 @@
 package ;
 
+import haxe.Unserializer;
+
 /**
  * シリアライズされた文字列をJSON形式に整形し、
- * テキストファイルで出力するクラス
+ * テキストファイルで出力する
  */
 class SerializationReader {
 	private var serializedData : String;
@@ -11,4 +13,19 @@ class SerializationReader {
 	public function new(s:String) {
 		this.serializedData = s;
 	}	
+	/**
+	 * デシリアライズされたオブジェクト型の変数をJson形式にして出力
+	 * 返り値をStringにするように修正予定。
+	 * @param	unserializedObjectData デシリアライズされたオブジェクト型の変数
+	 */
+	public static function traceObjectJsonData(unserializedObjectData : Dynamic) : Void {
+		var fields = Reflect.fields(unserializedObjectData);
+		
+		trace("\n{");
+		for (field in fields) {
+			var reflectField = Reflect.field(unserializedObjectData, field);
+			trace('"$field" : $reflectField - type,');
+		}
+		trace("\n}");
+	}
 }
