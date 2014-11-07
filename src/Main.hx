ec6be4db7d6fc1c8e1d153aba87777e04793c731
000ubird <000ubird@gmail.com>
2014-11-07 19:55:48 +0900

    マップ・ハッシュの文字列生成メソッドのテストをMainメソッドに実装。
    入れ子構造のマップ変数は　[ hoge --> [ bar --> [foo --> 51] ] ]　の様に出力される。

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,6 @@
 package ;
+import haxe.Serializer;
+import haxe.Unserializer;
 
 class Main {
 	
@@ -10,6 +12,22 @@ class Main {
 		var exampleSerializedData4 = 'oy1:bcy16:flash.geom.Pointy1:xzy1:yi100gy1:ewy5:Colory3:Rgb:1i10y1:ccy17:flash.geom.Matrixy2:txzy1:ai1y2:tyzR0zR7zy1:di1gR10ai1i2i3hR12wR5y3:Red:0g';
 		var exampleSerializedData5 = 'by3:YESi123h';
 		
+		var m:Map<Int, String> = [
+            1 => "One",
+            2 => "Two",
+            3 => "Three",
+        ];
+		var hash = Unserializer.run(exampleSerializedData5);
+		var map1 = new Map();
+		var map2 = new Map();
+		var map3 = new Map();
+		map1.set("foo", 51);
+		map2.set("bar", map1);
+		map3.set("hoge", map2);
+		trace(SerializationReader.getStringMapContents(map3));
+		
+		/*
+		var exampleSerializedData6 = Serializer.run(map3);
 		trace(ExtendedUnserializer.run(exampleSerializedData5));
 		//デシリアライズした整形前のデータを出力
 		trace(exampleSerializedData1);
@@ -17,6 +35,7 @@ class Main {
 		trace(exampleSerializedData3);
 		trace(exampleSerializedData4);
 		trace(exampleSerializedData5);
+		trace(exampleSerializedData6);
 		
 		//ファイル出力・読み込みのテスト
 		var unserialData = ExtendedUnserializer.run(exampleSerializedData4);
@@ -25,5 +44,6 @@ class Main {
 		SerializationReader.outputString(out, "out.txt");
 		var read = SerializationReader.readTextFile("out.txt");
 		trace(SerializationReader.getOriginalUnserializedData(read));
+		*/
 	}
 }
