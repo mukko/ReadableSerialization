95357591f88c4e29ac630645026d41bb5b524b93
000ubird <000ubird@gmail.com>
2014-12-02 01:23:52 +0900

    Enumの出力書式を変更
    「"Enum名" : SEnum("コンストラクタ") = パラメータ」とする。
    パラメータは再帰的に整形シリアライズ文字列を取得させる。

--- src/SerializationReader.hx
+++ src/SerializationReader.hx
@@ -86,10 +86,10 @@ class SerializationReader {
 			}
 		case SValueType.SEnum : 
 			var dummyEnum : DummyEnum = unserializedData;
-			var array = dummyEnum.getParameters();
+			var enumParam = dummyEnum.getParameters();
 			//Enumのパラメータを引数に取り、再帰的に呼び出し
-			buf += getShapedSerializeData(array[2],indent) + "\n";
-			
+			buf += '"'+enumParam[0]+'"'+" : SEnum("+enumParam[1]+") = "+getShapedSerializeData(enumParam[2],indent)+"\n";
+		
 		default : 
 			buf += '"__type_name__" : $type = $unserializedData\n';
 		}
