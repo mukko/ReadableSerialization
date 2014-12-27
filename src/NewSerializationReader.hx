e88b3ef872dd9e51f374a12b7a53ff55e40193f7
000ubird <000ubird@gmail.com>
2014-12-27 18:01:52 +0900

    コンストラクタを定義
    String型のシリアライズ文字列を引数に取り、拡張デシリアライザを利用して拡張デシリアライズデータにする。
    シリアライズ文字列と拡張デシリアライズデータはフィールド変数に保持する。

--- src/NewSerializationReader.hx
+++ src/NewSerializationReader.hx
@@ -9,4 +9,14 @@ class NewSerializationReader {
 	public var extendedUnserializedData(default, null) : Dynamic;	//拡張デシリアライザデータ
 	private var indent : Int = 0;									//整形時のインデント保持用変数
 	private static var INDENT = "	";								//スペース4個分のインデント文字列
+	
+	/**
+	 * シリアライズ文字列を引数に取る
+	 * 引数のシリアライズ文字列を拡張デシリアライザデータにしてフィールド変数に保持
+	 * @param	serializedText シリアライズ文字列
+	 */
+	public function new(serializedText : String) {
+		this.serializedText = serializedText;
+		extendedUnserializedData = ExtendedUnserializer.run(serializedText);
+	}
 }
