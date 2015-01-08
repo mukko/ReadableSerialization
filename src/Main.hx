30955232393ded5923b68a29a043e7059a6da551
000ubird <000ubird@gmail.com>
2015-01-08 23:48:42 +0900

    コマンドラインからシリアライズ文字列の書かれたファイルを読み込ませ、整形シリアライズデータを取得できるように変更
    Mainクラスにmcliを継承させた。
    指定したファイルが無い場合とファイル名を指定していない場合はエラー文字列を出力する。

--- src/Main.hx
+++ src/Main.hx
@@ -2,64 +2,27 @@ package ;
 import haxe.Serializer;
 import haxe.Unserializer;
 
-class Main {
-	private static var FILENAME = "sample.txt";
+class Main extends mcli.CommandLine {
+	
+	public function runDefault(?name:String) {
+		if (name == null) {
+			Sys.println("No files selected.");
+		}
+		else {
+			//指定したファイルからシリアライズ文字列を取得
+			var sr = SerializationReader.readTextFile(name);
+			if (sr == null) {
+				Sys.println("No such file "+name);
+			}
+			else {
+				var sr = new NewSerializationReader(sr);
+				SerializationReader.outputString(sr.run(),"out_"+name+".txt");
+				Sys.println("Save as out_"+name+".txt");
+			}
+		}
+	}
 	
 	public static function main() {
-		//デシリアライズした結果を変数に格納
-		var exampleSerializedData1 = 'cy17:flash.geom.Matrixy1:ai1y2:txi100y1:bzy2:tyi200y1:czy1:di1g';
-		var exampleSerializedData2 = 'wy17:com.example.Colory3:Rgb:1i128';
-		var exampleSerializedData3 = 'awy5:Colory3:Rgb:1i128wR0y3:Red:0h';
-		var exampleSerializedData4 = 'oy1:bcy16:flash.geom.Pointy1:xzy1:yi100gy1:ewy5:Colory3:Rgb:1i10y1:ccy17:flash.geom.Matrixy2:txzy1:ai1y2:tyzR0zR7zy1:di1gR10ai1i2i3hR12wR5y3:Red:0g';
-		var exampleSerializedData5 = 'by3:YESi123h';
-		var exampleSerializedData6 = SerializationReader.readTextFile(FILENAME);
-		
-		//マップ変数のテスト
-		var m:Map<Int, String> = [
-            1 => "One",
-            2 => "Two",
-            3 => "Three",
-        ];
-		var map1 = new Map();
-		var map2 = new Map();
-		var map3 = new Map();
-		
-		var obj0 = {
-			i : 500,
-			j : 100,
-		}
-		var obj1 = {
-			a : 10,
-			b : 20,
-			o : obj0,
-		}
-		var obj2 = {
-            x: 120,
-            y: 230,
-            z: obj1,
-			array: [10, 20],
-			bool : true,
-			str : "test",
-			hash : m,
-			food: Food.Etc("appple", "good", 100, Hamburg),
-			food2 : Food.Banana,
-        }
-		var enumTest1 = Food.Etc("appple", "good", 100, Food.Etc("pop","aaaa",50,Food.Etc("a","b",1,Banana)));
-		var enumTest2 = Food.Banana;
-		var array2: Array<Dynamic> = [1,null,null,248.55,"test"];
-		var array : Array<Dynamic> = [10, 10.59, "aabb",obj2,enumTest1];
-		var d:Dynamic = { "ten":"10", "twenty":"20" };
-		var str = "aaabbb";
-		map1.set("foo", obj2);
-		map2.set("bar", map1);
-		map3.set("hoge", map2);
-		var exampleSerializedData7 = Serializer.run(array);
-		//trace(enumTest1);
-		/*var sss = ExtendedUnserializer.run(exampleSerializedData6);
-		var out = SerializationReader.getShapedSerializeData(sss, 0);
-		SerializationReader.outputString(out, "out_new.txt");*/
-		var sr = new NewSerializationReader(exampleSerializedData6);
-		var out = sr.run();
-		SerializationReader.outputString(out, "out_new.txt");
+		new mcli.Dispatch(Sys.args()).dispatch(new Main());
 	}
 }
