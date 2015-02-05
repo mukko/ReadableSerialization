56427db3d43b6bdcdf1a31e7bf0e88586f394ad3
000ubird <000ubird@gmail.com>
2015-02-06 01:41:59 +0900

    【WIP】1行分の整形シリアライズデータから型情報を取り出すメソッドを定義
    Class型とEnum型の場合の処理は未実装

--- src/SerializationWriter.hx
+++ src/SerializationWriter.hx
@@ -1,4 +1,5 @@
 package ;
+import Type;
 
 /**
  * 整形シリアライズデータを元に元のデータを生成する
@@ -18,4 +19,35 @@ class SerializationWriter {
 		readableSerializedText = FileTools.readTextFile(filename);
 	}
 	
+	
+	/**
+	 * 整形シリアライズ文字列の1行分のデータから型情報を取り出す
+	 * @return データの型
+	 */
+	private function typeof() : ValueType {
+		var r : EReg = ~/:.*=/;	//型名を取り出す正規表現
+		r.match(line);
+		var type = r.matched(0);	//正規表現によって抽出された文字列を保持
+		type = StringTools.replace(type, ':','');	//「:」の削除
+		type = StringTools.replace(type, '=', '');	//「=」の削除
+		type = StringTools.replace(type, ' ', '');	//スペースの削除
+		
+		//型情報を元にTypeのEnumコンストラクタを返す
+		switch (type) {
+			case "SNull"	: return TNull;
+			case "SInt"		: return TInt;
+			case "SFloat"	: return TFloat;
+			case "SBool"	: return TBool;
+			case "SObject"	: return TObject;
+			case "SFunction": return TFunction;
+			case "SArray"	: return TClass(Array);
+			case "SString"	: return TClass(String);
+			case "SIntMap"		: return TClass(haxe.ds.IntMap);
+			case "SStringMap"	: return TClass(haxe.ds.StringMap);
+			case "SEnumValueMap": return TClass(haxe.ds.EnumValueMap);
+			case "SObjectMap"	: return TClass(haxe.ds.ObjectMap);
+			case "SUnknown" : return TUnknown;
+			default			: return TUnknown;
+		}
+	}
 }
