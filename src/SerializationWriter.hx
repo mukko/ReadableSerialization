e01aac87ab0788844b8402e4c37af3ccff58a4fa
000ubird <000ubird@gmail.com>
2015-02-07 23:38:12 +0900

    フィールド変数を修正・削除
    ・整形シリアライズデータを保持するフィールド変数を削除
    ・シリアライズ元のデータを保持するフィールド変数のスコープを変更
    ・ファイル名を保持するフィールド変数を追加
    ・読み取る行数を保持するフィールド変数を追加
    
    ・コンストラクタは読み取るファイル名を保持するように変更

--- src/SerializationWriter.hx
+++ src/SerializationWriter.hx
@@ -6,17 +6,18 @@ import Type;
  * @author 000ubird
  */
 class SerializationWriter {
-	public var readableSerializedText(default, null) : String;	//整形シリアライズ文字列
-	private var originalValue : Dynamic;//シリアライズ元のデータ
+	public var originalValue(default,null) : Dynamic;	//シリアライズ元のデータ
+	private var fileName : String = "";	//整形シリアライズデータの書かれたテキストファイル名
 	private var line : String = "";		//整形シリアライズ文字列の1行を保持
+	private var currentLine : Int = 0;		//読み取る行数
 	
 	/**
 	 * 整形シリアライズデータの文字列が書かれたテキストファイル名を
-	 * 引数に取り、文字列を読み込んでフィールドに保持
+	 * 引数に取ってフィールドに保持する
 	 * @param	filename 読み込むテキストファイル名
 	 */
-	public function new(filename : String) {
-		readableSerializedText = FileTools.readTextFile(filename);
+	public function new(fileName : String) {
+		this.fileName = fileName;
 	}
 	
 	/**
