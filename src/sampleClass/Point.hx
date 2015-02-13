079508c3401d90904289deac57817cebe4b4fbf3
000ubird <000ubird@gmail.com>
2015-02-13 21:10:08 +0900

    クラスの自動生成テスト用のサンプルクラスを追加し、Mainメソッド内に試験的に生成テストを実装

---
+++ src/sampleClass/Point.hx
@@ -0,0 +1,15 @@
+package sampleClass;
+
+/**
+ * サンプルクラスその①
+ * @author 000ubird
+ */
+class Point {
+	public var x(default, null) : Int;
+	public var y(default, null) : Int;
+	
+	public function new(x : Int , y : Int) {
+		this.x = x;
+		this.y = y;
+	}	
+}
--- src/serializationWriter/Main.hx
+++ src/serializationWriter/Main.hx
@@ -1,21 +1,29 @@
 package serializationWriter;
+
 import haxe.Serializer;
 import haxe.Unserializer;
+import sampleClass.Point;
 
 class Main {
-	
 	public static function main() {
-		var className = "";
+		var className = 'sampleClass.Point';
 		var resolveClass = Type.resolveClass(className);
 		if (resolveClass == null) throw 'class not found';
-		trace(resolveClass);	//デバッグ
+		//デバッグ
+		trace(resolveClass);	
+		/*{ __name__ => [sampleClass,Point],
+		 * __construct__ => #function:2, 
+		 * prototype => { __class__ => ..., x => null, y => null,
+		 * __serialize => #function:0 },
+		 * new => #function:2 }
+		*/
 		
 		//フィールド変数を代入
 		var originalClassSample = Type.createEmptyInstance(resolveClass);
 		Reflect.setField(originalClassSample, "x", 10);
 		Reflect.setField(originalClassSample, "y", 20);
-		Reflect.setField(originalClassSample, "x", 30);
-		
-		trace(originalClassSample);	//デバッグ
+		Reflect.setField(originalClassSample, "z", 30);
+		//デバッグ
+		trace(originalClassSample);		//{ x => 10, y => 20, z => 30 }
 	}
 }
