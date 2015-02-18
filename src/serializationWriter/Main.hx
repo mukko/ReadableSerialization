8503f225fc5f97824a8734974df64228a816adc9
000ubird <000ubird@gmail.com>
2015-02-18 23:56:57 +0900

    Mainメソッドをに元データ生成処理を実装
    クラスの生成テストに関しての実装を削除し、元データ生成処理を実装

--- src/serializationWriter/Main.hx
+++ src/serializationWriter/Main.hx
@@ -2,28 +2,14 @@ package serializationWriter;
 
 import haxe.Serializer;
 import haxe.Unserializer;
-import sampleClass.Point;
 
 class Main {
 	public static function main() {
-		var className = 'sampleClass.Point';
-		var resolveClass = Type.resolveClass(className);
-		if (resolveClass == null) throw 'class not found';
+		//試験的な元データ生成
+		var sw = new SerializationWriter("classSample.txt");
+		var originValue = sw.run();
 		//デバッグ
-		trace(resolveClass);	
-		/*{ __name__ => [sampleClass,Point],
-		 * __construct__ => #function:2, 
-		 * prototype => { __class__ => ..., x => null, y => null,
-		 * __serialize => #function:0 },
-		 * new => #function:2 }
-		*/
-		
-		//フィールド変数を代入
-		var originalClassSample = Type.createEmptyInstance(resolveClass);
-		Reflect.setField(originalClassSample, "x", 10);
-		Reflect.setField(originalClassSample, "y", 20);
-		Reflect.setField(originalClassSample, "z", 30);
-		//デバッグ
-		trace(originalClassSample);		//{ x => 10, y => 20, z => 30 }
+		trace(originValue);
+		trace(Type.typeof(originValue));
 	}
 }
