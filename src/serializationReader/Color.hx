0cda6a75f2c96d3f051f66ac8650ebfe141dfa53
000ubird <000ubird@gmail.com>
2015-04-24 15:20:34 +0900

    サンプルのEnumのコンストラクタを修正

--- src/serializationReader/Color.hx
+++ src/serializationReader/Color.hx
@@ -5,6 +5,6 @@ enum Color {
 	Green;
 	Blue;
 	Grey( v : Int );
-	Rgb( r : Int, g : Int, b : Color3 );
-	Rgb2(r : Int, g : Color3, b : Int);
+	Rgb( r : Int, g : Int, b : Color );
+	Rgb2(r : Int, g : Color, b : Int);
 }
