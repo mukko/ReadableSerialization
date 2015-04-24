2f86f89ad531bd8b2a294216a1899eb7d641eabb
mukko <mukk0.aquarius@gmail.com>
2015-04-24 11:31:08 +0900

    sampleClassのPointにenumを追加

--- src/sampleClass/Point.hx
+++ src/sampleClass/Point.hx
@@ -7,9 +7,11 @@ package sampleClass;
 class Point {
 	public var x(default, null) : Int;
 	public var y(default, null) : Int;
+	public var e(default, null) : Enum;
 	
-	public function new(x : Int , y : Int) {
+	public function new(x : Int , y : Int, e : Enum) {
 		this.x = x;
 		this.y = y;
+		this.e = e;
 	}	
 }
