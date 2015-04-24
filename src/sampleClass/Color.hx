7679ba1da41377e3b1bc4a6e9c9d5af3cf40bd87
mukko <mukk0.aquarius@gmail.com>
2015-04-24 16:29:01 +0900

    Enum中にパターンを作成

--- src/sampleClass/Color.hx
+++ src/sampleClass/Color.hx
@@ -1,10 +1,14 @@
 package sampleClass;
 
+import haxe.ds.ObjectMap;
 enum Color {
 	Red;
 	Green;
 	Blue;
 	Grey( v : Int );
-	Rgb( r : Int, g : Int, b : Color);
-	Rgb2(r : Int, g : Color, b : Int);
+	testRgb( r : Int, g : Int, b : Color); //Enum内にEnum
+	testClass( c : Primitive); //Enum内にクラス
+	testArray( a : Array<Dynamic>); //Enum内に配列
+	testMap( m : ObjectMap<Dynamic, Dynamic>); //Enum内にマップ
+	testObject( o : Dynamic); //Enum内にオブジェクト 
 }
