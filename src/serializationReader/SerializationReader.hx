989455cd7a7c72fffc3b0a47b6f8abe2c135cade
000ubird <000ubird@gmail.com>
2015-04-27 15:31:09 +0900

    シリアライズリーダのマップ整形メソッドで、括弧とカンマが正しく出力されなかった問題を修正

--- src/serializationReader/SerializationReader.hx
+++ src/serializationReader/SerializationReader.hx
@@ -225,28 +225,20 @@ class SerializationReader {
 			if (isRecursive(keyType)) {
 				//値の型が再帰が必要な場合、値を引数に入れて再帰的に呼び出す.
 				if (isRecursive(valueType)) {
-					strBuf.add('( __mapKey : $keyType = '+getReadableSerializedText(key)+' -> __mapValue : $valueType = ' +getReadableSerializedText(map.get(key)));
+					strBuf.add('( __mapKey : $keyType = '+getReadableSerializedText(key)+' -> __mapValue : $valueType = ' +getReadableSerializedText(map.get(key))+'),');
 				}
 				else {
-					strBuf.add('( __mapKey : $keyType = '+getReadableSerializedText(key)+' -> __mapValue : $valueType = '+map.get(key));
+					strBuf.add('( __mapKey : $keyType = '+getReadableSerializedText(key)+' -> __mapValue : $valueType = '+map.get(key)+'),');
 				}
 			}
 			else {
 				if (isRecursive(valueType)) {
-					strBuf.add('( __mapKey : $keyType = $key -> __mapValue : $valueType = ' +getReadableSerializedText(map.get(key)));
+					strBuf.add('( __mapKey : $keyType = $key -> __mapValue : $valueType = ' +getReadableSerializedText(map.get(key))+'),');
 				}
 				else {
-					strBuf.add('( __mapKey : $keyType = $key -> __mapValue : $valueType = '+map.get(key));
+					strBuf.add('( __mapKey : $keyType = $key -> __mapValue : $valueType = '+map.get(key)+'),');
 				}
 			}
-		}
-		
-		//再帰で呼び出された場合はカンマを出力しない
-		if (recursiveDepth >= NOT_OUTPUT_VALUE_TYPE) {
-			strBuf.add(')');
-		}
-		else {
-			strBuf.add('),');
 		}
 		
 		return strBuf.toString();
